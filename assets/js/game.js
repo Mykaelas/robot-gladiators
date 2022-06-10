1b25b9e0d71ea33e51c509a707cd328087516bf8
Mykaela Saenz <mickycore@gmail.com>
2022-06-09 22:53:25 -0500

    Added null prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,9 +7,41 @@ var randomNumber = function(min, max) {
   return value;
 };
 
+var fightOrSkip = function() {
+  //ask player if they'd like to fight or skip using fightOrSkip function
+
+  var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+// Conditional Recursive Function Call
+
+  if (promptFight === "" || promptFight === null) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return fightOrSkip();
+  }
+
+// if player picks "skip" confirm and thens top the loop
+
+  promptFight = promptFight.toLowerCase();
+
+  if (promptFight === "skip" || promptFight === "SKIP") {
+    // confirm player wants to skip
+    var confirmSkip = window.confirm ("Are you sure you'd like to quit");
+
+    // if yes (true), leave fight
+  if (confirmSkip) {
+    window.alert(playerInfo.name + "has decided to skip this fight. Goodbye!");
+    // subtract money from playerMoney for skipping
+    playerInfo.playerMoney = playerInfo.money - 10;
+    shop()
+    }
+  }
+}
 // fight function (now with parameter for enemy's object holding name, health, and attack values)
 var fight = function(enemy) {
   while (playerInfo.health > 0 && enemy.health > 0) {
+
+    fightOrSkip (); //<-- Replace code with this function call
+    var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
     // ask player if they'd like to fight or run
     var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
@@ -23,8 +55,8 @@ var fight = function(enemy) {
         window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerInfo.money for skipping
         playerInfo.money = Math.max(0, playerInfo.money - 10);
-        console.log("playerInfo.money", playerInfo.money)
-        break;
+
+        return true;
       }
     }
 
@@ -167,7 +199,25 @@ var shop = function() {
 
 /* END GAME FUNCTIONS */
 
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null {
+    name = prompt("What is your robot's name?");
+  }
+
+  var playerInfo = {
+    name:getPlayerName (),
+    ..// other playerInfo properties and methods
+
+  }
+  
+  console.log("Your robot's name is " + name );
+  return name;
+  
+};
 /* GAME INFORMATION / VARIABLES */
+
 
 // player information
 var playerInfo = {
